4cf03ea8afb17bae0e6bd6dd5ee8f32d747ac277
Kenneth Seet <kenurfseet@gmail.com>
2024-09-20 20:32:03 -0700

    Touch up endpoints

--- frontend/src/routes/cafeRoutes.ts
+++ frontend/src/routes/cafeRoutes.ts
@@ -10,10 +10,10 @@ import {
 const router: Router = Router();
 
 // Define routes for cafes
-router.post("/", createCafe); // Create a new cafe
-router.get("/", getCafesByLocation); // Get all cafes or filter by location if provided
-router.get("/:id", getCafeById); // Get a specific cafe by ID
-router.put("/:id", updateCafe); // Update a specific cafe by ID
-router.delete("/:id", deleteCafe); // Delete a specific cafe by ID
+router.post("/", createCafe);
+router.get("/", getCafesByLocation);
+router.get("/:id", getCafeById);
+router.put("/:id", updateCafe);
+router.delete("/:id", deleteCafe);
 
 export default router;
