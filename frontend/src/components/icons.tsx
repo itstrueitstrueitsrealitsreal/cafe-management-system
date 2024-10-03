e3c65cfbe71f5193bdaacdb438ae9a1620c822bf
Kenneth Seet <kenurfseet@gmail.com>
2024-10-03 13:00:13 -0700

    Complete navbar

--- frontend/src/components/icons.tsx
+++ frontend/src/components/icons.tsx
@@ -23,6 +23,31 @@ export const Logo: React.FC<IconSvgProps> = ({
   </svg>
 );
 
+export const CoffeeIcon: React.FC<IconSvgProps> = ({
+  size = 24,
+  width,
+  height,
+  fill = "currentColor",
+  ...props
+}) => (
+  <svg
+    enableBackground="new 0 0 24 24"
+    fill={fill}
+    height={size || height}
+    viewBox="0 0 24 24"
+    width={size || width}
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g>
+      <path d="M0,0h24v24H0V0z" fill="none" />
+    </g>
+    <g>
+      <path d="M18.5,3H6C4.9,3,4,3.9,4,5v5.71c0,3.83,2.95,7.18,6.78,7.29c3.96,0.12,7.22-3.06,7.22-7v-1h0.5c1.93,0,3.5-1.57,3.5-3.5 S20.43,3,18.5,3z M16,5v3H6V5H16z M18.5,8H18V5h0.5C19.33,5,20,5.67,20,6.5S19.33,8,18.5,8z M4,19h16v2H4V19z" />
+    </g>
+  </svg>
+);
+
 export const DiscordIcon: React.FC<IconSvgProps> = ({
   size = 24,
   width,
--- frontend/src/config/site.ts
+++ frontend/src/config/site.ts
@@ -10,23 +10,6 @@ export const siteConfig = {
       href: "/",
     },
     {
-      label: "Docs",
-      href: "/docs",
-    },
-    {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
-      label: "Blog",
-      href: "/blog",
-    },
-    {
-      label: "About",
-      href: "/about",
-    },
-    // Adding cafe routes
-    {
       label: "Cafes",
       href: "/cafes",
     },
@@ -37,39 +20,15 @@ export const siteConfig = {
   ],
 
   navMenuItems: [
-    {
-      label: "Profile",
-      href: "/profile",
-    },
-    {
-      label: "Dashboard",
-      href: "/dashboard",
-    },
-    {
-      label: "Projects",
-      href: "/projects",
-    },
-    {
-      label: "Team",
-      href: "/team",
-    },
-    {
-      label: "Calendar",
-      href: "/calendar",
-    },
-    {
-      label: "Settings",
-      href: "/settings",
-    },
-    {
-      label: "Help & Feedback",
-      href: "/help-feedback",
-    },
-    {
-      label: "Logout",
-      href: "/logout",
-    },
     // Adding more specific cafe-related actions
+    {
+      label: "Cafes",
+      href: "/cafes",
+    },
+    {
+      label: "Employees",
+      href: "/employees",
+    },
     {
       label: "Add Cafe",
       href: "/cafes/add",
@@ -81,10 +40,7 @@ export const siteConfig = {
   ],
 
   links: {
-    github: "https://github.com/nextui-org/nextui",
-    twitter: "https://twitter.com/getnextui",
-    docs: "https://nextui-docs-v2.vercel.app",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
+    github:
+      "https://github.com/itstrueitstrueitsrealitsreal/cafe-management-system",
   },
 };
