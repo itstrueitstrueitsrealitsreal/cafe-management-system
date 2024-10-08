d76e270ab2543b26ac73918ba04033e956f4cc55
Kenneth Seet <kenurfseet@gmail.com>
2024-10-08 16:01:04 -0700

    Complete frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,30 @@
-import { defineConfig } from "vite";
+import { resolve } from "path";
+
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    host: true,
-    port: 5173,
-    proxy: {
-      // Proxy requests to `/api` to the backend server
-      "/api": {
-        target: "http://localhost:3000", // The backend server URL
-        changeOrigin: true, // Needed for virtual hosted sites
-        rewrite: (path) => path.replace(/^\/api/, ""), // Optional: If the backend doesn't need the `/api` prefix
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: "/",
+    plugins: [react(), tsconfigPaths()],
+    server: {
+      host: true,
+      port: 5173,
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_URL || "http://localhost:3000",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      },
+    },
+  };
 });
