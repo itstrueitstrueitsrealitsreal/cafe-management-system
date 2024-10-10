3fe2dffa017063f9e7b91b45d24114d605a1a15c
Kenneth Seet <kenurfseet@gmail.com>
2024-10-10 01:43:23 -0700

    Adjust cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,11 +11,43 @@ dotenv.config();
 const app: Application = express();
 
 connectDB();
-app.use(cors());
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || [
+  "http://localhost:5173",
+  "https://cafe-management-system-nine.vercel.app",
+];
+const corsOptions = {
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.use("/", defaultRoutes);
 app.use("/employees", employeeRoutes);
 app.use("/cafes", cafeRoutes);
+
+app.use(
+  (
+    err: any,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    console.error(err.stack);
+    res.status(500).send("Something broke!");
+  }
+);
 
 export default app;
